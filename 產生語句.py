e8528e3251083dd26e81e6dc93b439d1aba40f2b
Fafofafoy <ihcaoe@gmail.com>
2019-06-18 17:10:48 +0800

    Sing phue-hap tau-phah-ji ing soo-ji-tiau.

--- 產生語句.py
+++ 產生語句.py
@@ -2,6 +2,8 @@
 
 from django.core.management.base import BaseCommand
 from 臺灣言語服務.models import 訓練過渡格式
+from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
+from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
 
 
 class Command(BaseCommand):
@@ -9,6 +11,10 @@ class Command(BaseCommand):
     def handle(self, *args, **參數):
         ku = set()
         for tsuliau in 訓練過渡格式.objects.all():
-            ku.add(tsuliau.文本)
+            ku.add(
+                拆文分析器.分詞句物件(tsuliau.文本)
+                .轉音(臺灣閩南語羅馬字拼音)
+                .看分詞()
+            )
         with open("ku.txt", 'w') as tong:
             print('\n'.join(sorted(ku)), file=tong)
