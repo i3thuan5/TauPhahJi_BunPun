b5d1538c0e8b368272c8b4a005dd65ef9f6e8af9
Fafofafoy <ihcaoe@gmail.com>
2019-06-20 18:27:18 +0800

    Ka Kam-ji-tian.

--- 產生辭典.py
+++ 產生辭典.py
@@ -1,5 +1,6 @@
-from 用字.教典規範 import 教典
-from 用字.標點規範 import 標點
+from 用字 import 教典
+from 用字 import 標點
+from 用字 import 甘字典
 from 臺灣言語工具.音標系統.閩南語.臺灣閩南語羅馬字拼音 import 臺灣閩南語羅馬字拼音
 from 臺灣言語工具.解析整理.拆文分析器 import 拆文分析器
 from sys import stdout, stdin
@@ -10,6 +11,8 @@ def 用字會使無(詞物件):
         if 教典.有這个字無(字物件):
             continue
         if 標點.有這个字無(字物件):
+            continue
+        if 甘字典.有這个字無(字物件):
             continue
         if (
             字物件.型 == 字物件.音
